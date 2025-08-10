6b943638feca94b16156946dc77e3bb380de9f39
Santeri Heiskanen <heiskanens98@gmail.com>
2025-08-10 19:18:27 +0300

    Update example to work with never requirements

--- nasbench301/example.py
+++ nasbench301/example.py
@@ -4,31 +4,41 @@ from collections import namedtuple
 from ConfigSpace.read_and_write import json as cs_json
 
 import nasbench301 as nb
+import argparse
+import pathlib
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_dir", type=pathlib.Path, default=pathlib.Path(__file__).parent)
+parser.add_argument("--config_space_dir", type=pathlib.Path, default=pathlib.Path(__file__).parent)
+
+args = parser.parse_args()
 
 # Default dirs for models
 # Note: Uses 0.9 as the default models, switch to 1.0 to use 1.0 models
-version = '0.9'
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-models_0_9_dir = os.path.join(current_dir, 'nb_models_0.9')
-model_paths_0_9 = {
-    model_name : os.path.join(models_0_9_dir, '{}_v0.9'.format(model_name))
-    for model_name in ['xgb', 'gnn_gin', 'lgb_runtime']
-}
-models_1_0_dir = os.path.join(current_dir, 'nb_models_1.0')
+# models_0_9_dir = os.path.join(current_dir, 'nb_models_0.9')
+# model_paths_0_9 = {
+#     model_name : os.path.join(models_0_9_dir, '{}_v0.9'.format(model_name))
+#     for model_name in ['xgb', 'gnn_gin', 'lgb_runtime']
+# }
+
+
+
 model_paths_1_0 = {
-    model_name : os.path.join(models_1_0_dir, '{}_v1.0'.format(model_name))
-    for model_name in ['xgb', 'gnn_gin', 'lgb_runtime']
+    model_name: (args.model_dir / "{}_v1.0".format(model_name)) 
+        for model_name in ["xgb", "lgb_runtime"]
 }
-model_paths = model_paths_0_9 if version == '0.9' else model_paths_1_0
+
+model_paths = model_paths_1_0
+
 
 # If the models are not available at the paths, automatically download
 # the models
 # Note: If you would like to provide your own model locations, comment this out
-if not all(os.path.exists(model) for model in model_paths.values()):
-    nb.download_models(version=version, delete_zip=True,
-                       download_dir=current_dir)
-
+# if not all(os.path.exists(model) for model in model_paths.values()):
+#     nb.download_models(version=version, delete_zip=True,
+#                        download_dir=current_dir)
+#
 # Load the performance surrogate model
 #NOTE: Loading the ensemble will set the seed to the same as used during training (logged in the model_configs.json)
 #NOTE: Defaults to using the default model download path
@@ -53,12 +63,16 @@ genotype_config = Genotype(
         reduce_concat=[2, 3, 4, 5]
         )
 
+print(f"Genotype {genotype_config}")
 # Option 2: Sample from a ConfigSpace
-configspace_path = os.path.join(current_dir, 'configspace.json')
+
+configspace_path = args.config_space_dir  / "configspace.json"
+#configspace_path = os.path.join(current_dir, 'configspace.json')
 with open(configspace_path, "r") as f:
     json_string = f.read()
     configspace = cs_json.read(json_string)
 configspace_config = configspace.sample_configuration()
+print(f"config_space {configspace_config}")
 
 # Predict
 print("==> Predict runtime and performance...")
